3a4d1ddee07df51a75e9057e332003cdd4360a96
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-01-15 00:55:48 +0300

    Add division

--- src/app/Main.java
+++ src/app/Main.java
@@ -29,4 +29,8 @@ public class Main {
     private static int subtraction(int a, int b) {
         return a - b;
     }
+
+    private static int division(int a, int b) {
+        return a / b;
+    }
 }
